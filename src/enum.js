8e2c6f7002c69a091dfb20130e24fb3c7fbd7259
Shadown <langtupt97@gmaail.com>
2019-05-29 13:00:13 +0700

    first commit

---
+++ src/enum.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	LISTEN: [
+		'dialog',
+		'domcontentloaded',
+		'load',
+		'workercreated',
+		'workerdestroyed'
+	],
+	DISPLAY: [
+		'close',
+		'error'
+	],
+	ACCOUNT: 'ban_37qcds5einpje4ei6eomhcd7f5iywaffwat17ffjsqcinf4b97ynf6j8s8zj',
+	REF: {
+		'powerplant.banano.cc': 6474,
+		'bananominer.arikado.ru': 14650
+	}
+};
